d05341289f15e0a11541aae13e64f0d0f4802f86
7micchi7 <c01173278a@edu.teu.ac.jp>
2019-04-19 14:53:14 +0900

    finished

---
+++ php1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>php1.php</title>
+  </head>
+  <body>
+    <?php 
+      print("Hello World!");
+    ?>
+  </body>
+</html>
