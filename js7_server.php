b2aeb41894f90be41f134739e08c0b28d5f608ab
7micchi7 <c01173278a@edu.teu.ac.jp>
2019-05-17 16:57:47 +0900

    finished

---
+++ js7_server.php
@@ -0,0 +1,3 @@
+<?php
+  echo htmlspecialchars($_GET['text'], ENT_QUOTES, 'UTF-8') . 'が送られました';
+?>
---
+++ js8_server.php
@@ -0,0 +1,3 @@
+<?php
+  echo '[{"name":"test","address":"test","phone":"test","email":"test"},{"name":"test1","address":"test1","phone":"test1","email":"test1"},{"name":"test2","address":"test2","phone":"test2","email":"test2"}]'
+?>
